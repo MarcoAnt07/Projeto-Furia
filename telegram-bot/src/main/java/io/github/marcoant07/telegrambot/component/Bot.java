b3f3eeb8cd9ef72b6e11da771042ba19382a93f4
Marco Antonio <marcorodoi64@gmail.com>
2025-04-25 17:05:19 -0300

    Configurando bot e registrando na API do Telegram

--- telegram-bot/src/main/java/io/github/marcoant07/telegrambot/component/Bot.java
+++ telegram-bot/src/main/java/io/github/marcoant07/telegrambot/component/Bot.java
@@ -1,14 +1,31 @@
 package io.github.marcoant07.telegrambot.component;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+import java.util.Collections;
+
 
 @Component
 public class Bot extends TelegramLongPollingBot {
 
+    private final String botToken;
+    private final String botUsername;
+
+    public Bot(
+            @Value("${telegrambots.botToken}") String botToken,
+            @Value("${telegrambots.botUsername}") String botUsername
+    ) {
+        super(botToken);
+        this.botToken = botToken;
+        this.botUsername = botUsername;
+    }
 
     public void processNonCommandUpdate(Update update){
         if (update.hasMessage() && update.getMessage().hasText()){
@@ -27,11 +44,56 @@ public class Bot extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
+        if(update.hasMessage() && update.getMessage().hasText()){
+            String messageText = update.getMessage().getText();
+            Long chatId = update.getMessage().getChatId();
 
+            if(messageText.equals("/start")){
+                sendWelcomeMessage(chatId);
+            } else if (update.hasCallbackQuery()) {
+                String data = update.getCallbackQuery().getData();
+                chatId = update.getCallbackQuery().getMessage().getChatId();
+
+                if("botao_clicado".equals(data)){
+                    sendMessage(chatId, "Botão clicado");
+                }
+            }
+        }
+    }
+
+    private void sendWelcomeMessage(Long chatId) {
+
+        SendMessage message = new SendMessage();
+
+        message.setChatId(chatId);
+        message.setText("Bem vindo!");
+
+        InlineKeyboardButton button = new InlineKeyboardButton("Clique aqui");
+        button.setCallbackData("botao_clicado");
+
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        markup.setKeyboard(Collections.singletonList(Collections.singletonList(button)));
+
+        message.setReplyMarkup(markup);
+
+        try{
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void sendMessage(Long chatId, String text) {
+        SendMessage message = new SendMessage(chatId.toString(), text);
+        try {
+            execute(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public String getBotUsername() {
-        return "FuriaCS_testBot";
+        return botUsername;
     }
 }
---
+++ telegram-bot/src/main/java/io/github/marcoant07/telegrambot/configuration/BotConfig.java
@@ -0,0 +1,24 @@
+package io.github.marcoant07.telegrambot.configuration;
+
+import io.github.marcoant07.telegrambot.component.Bot;
+import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class BotConfig {
+
+    private final Bot bot;
+
+    public BotConfig(Bot bot){
+        this.bot = bot;
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
+        botsApi.registerBot(bot);
+    }
+}
